4c1e3f80d620c89823010ae486c7f363ed343ed6
Matthew Ito <mito@Helenas-MBP.lan>
2025-06-06 22:59:41 -0700

    Install tailwind

--- app.vue
+++ app.vue
@@ -1,6 +1,11 @@
 <template>
     <div>
-        <NuxtRouteAnnouncer />
-        <NuxtWelcome />
+        <h1 class="font-bold text-3xl underline">
+            Hello world!
+        </h1>
+        <button class="btn btn-secondary">
+            Oops
+        </button>
+        <Icon name="tabler:map" style="color: black" />
     </div>
 </template>
